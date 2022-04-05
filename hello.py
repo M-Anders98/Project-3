c35c51595d45e26ef72e886c8329bf81dafeb03f
ga220604 <ga220604@tamu.edu>
2022-04-05 11:59:24 -0500

    Python FLask linked to the html files

--- hello.py
+++ hello.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/quiz.html')
 def quiz():
     return render_template('quiz.html')
+
 @app.route('/recs.html')
 def recs():
     return render_template('recs.html')
+    
 @app.route('/index.html')
 def home2():
     return render_template('index.html')
