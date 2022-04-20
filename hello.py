db94266899b5b60e335be141aa14bd3f5984b241
aaronbarbosa <aaronbarbosa@tamu.edu>
2022-04-20 14:13:50 -0500

    added wikipedia api functionality (game summaries)

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import urllib.request, json
 
 import os
 app = Flask(__name__)
+data = {}
 
 @app.route('/')
 def home():
@@ -12,12 +13,60 @@ def home():
 def quiz():
     return render_template('quiz.html')
 
-@app.route('/get_data')
+@app.route('/get_data1')
 def stuff ():
     url = "https://api.rawg.io/api/games?key=39399cb9ce8f477899ff74f53e93338f"
     response = urllib.request.urlopen(url)
     data = response.read()
     data = json.loads(data.decode('utf-8'))
+    gamelist = []
+    count = 0
+    for x in data["results"]:
+            y = json.dumps(x)
+            count = 0
+            for k in x.values():
+                if count == 2:
+                    k = k.replace(" ", "_")
+                    gamelist.append(k)
+                count = count + 1
+    # countdesc = 0
+    # for x in data["results"]:
+    #         y = json.dumps(x)
+    #         count = 0
+    #         for k in x.values():
+    #             if count == 5:
+    #                 urlwiki = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
+    #                 urlwiki = urlwiki + gamelist[countdesc]
+    #                 responsewiki = urllib.request.urlopen(urlwiki)
+    #                 datawiki = responsewiki.read()
+    #                 datawiki = json.loads(datawiki.decode('utf-8'))
+    #                 pagenum = list(datawiki['query']['pages'])
+    #                 data['results'][2]['background-image'] = datawiki['query']['pages'][pagenum[0]]['extract']
+    #                 countdesc += 1
+    #             count += 1
+    for i in range(len(gamelist)):
+        urlwiki = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
+        urlwiki = urlwiki + gamelist[i]
+        responsewiki = urllib.request.urlopen(urlwiki)
+        datawiki = responsewiki.read()
+        datawiki = json.loads(datawiki.decode('utf-8'))
+        pagenum = list(datawiki['query']['pages'])
+        substtring = datawiki['query']['pages'][pagenum[0]]['extract']
+        if(len(datawiki['query']['pages'][pagenum[0]]['extract']) < 550):
+            #print("WRONG GAME!")
+            urlwiki = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
+            urlwiki = urlwiki + gamelist[i] + "_(video_game)"
+            responsewiki = urllib.request.urlopen(urlwiki)
+            datawiki = responsewiki.read()
+            datawiki = json.loads(datawiki.decode('utf-8'))
+            pagenum = list(datawiki['query']['pages'])  
+            #print(datawiki['query']['pages'][pagenum[0]]['extract'])
+            substtring = datawiki['query']['pages'][pagenum[0]]['extract']
+        data['results'][i]['background_image'] = substtring[:substtring.find('\n')]
+
+
+    
+
     return jsonify(data)
 
 
